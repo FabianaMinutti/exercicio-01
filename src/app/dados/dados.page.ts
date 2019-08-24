0e63dd13d079ef4bc1add6dd3d16a5b8cb5fd1a8
pos liberado <pospos@labcomp.usc>
2019-08-24 09:22:54 -0300

    add dados

--- src/app/dados/dados.page.ts
+++ src/app/dados/dados.page.ts
@@ -9,7 +9,13 @@ export class DadosPage implements OnInit {
   private user: object;
 
   constructor() {
-    this.user = {name: "Fabiana", idade: "24", email: "fabianarminutti@gmail.com", telefone: "(14) 99999-9999"}
+    this.user = {
+      name: "Fabiana", 
+      idade: "24", 
+      email: "fabianarminutti@gmail.com", 
+      telefone: "(14) 99999-9999"
+    }
+    
   }
 
   ngOnInit() {

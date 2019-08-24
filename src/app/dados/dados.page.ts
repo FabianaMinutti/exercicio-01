058067c7816d9b7346f61a3ff228186720e8a9b0
pos liberado <pospos@labcomp.usc>
2019-08-24 08:56:30 -0300

    first commit

---
+++ src/app/dados/dados.page.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dados',
+  templateUrl: './dados.page.html',
+  styleUrls: ['./dados.page.scss'],
+})
+export class DadosPage implements OnInit {
+  private user: object;
+
+  constructor() {
+    this.user = {name: "Fabiana", idade: "24", email: "fabianarminutti@gmail.com", telefone: "(14) 99999-9999"}
+  }
+
+  ngOnInit() {
+  }
+
+}
